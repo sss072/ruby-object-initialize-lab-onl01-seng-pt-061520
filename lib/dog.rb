aacc079050ddccec8bc237999504fbf233cc885e
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-06 23:28:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,11 @@
 class Dog 
-  def initialize(name)
+  def initialize(name, breed = "Mutt")
     @name = name 
   end
   def name 
     @name 
+  end 
+  def breed
+    @breed = breed 
   end
-  
+end
